7bd8891738093775d023d1a2dad68cd847273d76
rafaelmartinsdacosta <rafael_martins_da_costa@outlook.com>
2018-09-21 22:48:02 -0300

    Inclusão de classes base

--- src/main/java/br/com/hackathon/easymed/EasymedApplication.java
+++ src/main/java/br/com/hackathon/easymed/EasymedApplication.java
@@ -2,7 +2,9 @@ package br.com.hackathon.easymed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EasymedApplication {
 
