32449c1badba4c367b24c55424664d9ddf25158c
rafaelmartinsdacosta <rafael_martins_da_costa@outlook.com>
2018-09-22 00:33:32 -0300

    Inclusão de controller de pacientes

--- src/main/java/br/com/hackathon/easymed/repository/PacienteRepository.java
+++ src/main/java/br/com/hackathon/easymed/repository/PacienteRepository.java
@@ -1,9 +1,9 @@
 package br.com.hackathon.easymed.repository;
 
-import br.com.hackathon.easymed.model.Question;
+import br.com.hackathon.easymed.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
