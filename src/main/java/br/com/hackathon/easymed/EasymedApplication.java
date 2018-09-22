5539f0eddd44aada9ed40df0f4ba214cc46138e1
Surfista <lubanco@edu.unifil.br>
2018-09-21 22:30:14 -0300

    [EASYMED][SERVICE] Start projeto hackathon

---
+++ src/main/java/br/com/hackathon/easymed/EasymedApplication.java
@@ -0,0 +1,12 @@
+package br.com.hackathon.easymed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymedApplication.class, args);
+	}
+}
